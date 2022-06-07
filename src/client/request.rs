8c3be6904652a926208afe2e8ea578002731c65f
Ahmed Yasen <ahmedyasen8@gmail.com>
2022-06-07 09:55:14 +0200

    - build a separate client with request and respnose modules - build a server module [WIP]

---
+++ src/client/request.rs
@@ -0,0 +1,55 @@
+
+use crate::msg::Message;
+use serde::ser::Serialize;
+
+#[derive(Debug)]
+pub struct Request {
+    msg: Message,
+}
+
+impl Request {
+    pub fn new<C: Into<String>>(cmd: C) -> Self {
+        let mut msg = Message::default();
+        msg.command = cmd.into();
+
+        Self { msg }
+    }
+
+    pub fn destination(mut self, destination: u32) -> Self {
+        let mut destination = vec![destination];
+        self.msg.destination.append(&mut destination);
+
+        self
+    }
+
+    pub fn destinations(mut self, destinations: Vec<u32>) -> Self {
+        let mut destinations = destinations;
+        self.msg.destination.append(&mut destinations);
+
+        self
+    }
+
+    pub fn expiration(mut self, exp: usize) -> Self {
+        self.msg.expiration = exp;
+        self
+    }
+
+    pub fn args<A: AsRef<[u8]>>(mut self, args: A) -> Self {
+        let data = base64::encode(args.as_ref());
+        self.msg.data = data;
+        self
+    }
+
+    pub fn body(self) -> Message {
+        self.msg
+    }
+}
+
+impl Serialize for Request {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.msg.serialize(serializer)
+    }
+}
---
+++ src/client/response.rs
@@ -0,0 +1,42 @@
+use crate::msg::Message;
+use anyhow::{Context, Result};
+use async_stream::stream;
+use bb8_redis::{
+    bb8::{Pool, PooledConnection},
+    redis::AsyncCommands,
+    RedisConnectionManager,
+};
+use futures::Stream;
+
+pub struct Response {
+    pool: Pool<RedisConnectionManager>,
+}
+
+impl Response {
+    pub fn new(pool: Pool<RedisConnectionManager>) -> Self {
+        Self { pool }
+    }
+
+    async fn get_connection(&self) -> Result<PooledConnection<'_, RedisConnectionManager>> {
+        let conn = self
+            .pool
+            .get()
+            .await
+            .context("unable to retrieve a redis connection from the pool")?;
+
+        Ok(conn)
+    }
+
+    /// Get response from the remote/local twin
+    /// [call this after use the send function]
+    pub fn response<C: AsRef<str>>(self, ret: C) -> impl Stream<Item = Result<Message>> {
+        stream! {
+            let mut conn = self.get_connection().await.unwrap();
+            let msg: Result<Message> = conn
+            .lpop(ret.as_ref(), None)
+            .await
+            .context("failed to get a response message");
+            yield msg;
+        }
+    }
+}
