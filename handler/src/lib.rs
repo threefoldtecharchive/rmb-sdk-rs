5d49d0de7dea3e9c45a49dcdc933a975925ebbc0
Muhamad Azmy <muhamad@incubaid.com>
2022-06-14 15:23:28 +0200

    Async handler data (#5)
    * WIP: support app data
    
    * handler macro finalizaed
    
    * use visibility from handler fn
    
    * rebase with development

--- handler/src/lib.rs
+++ handler/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn};
+use syn::{parse_macro_input, FnArg, ItemFn};
 
 #[proc_macro_attribute]
 pub fn handler(_attr: TokenStream, input: TokenStream) -> TokenStream {
@@ -10,19 +10,36 @@ pub fn handler(_attr: TokenStream, input: TokenStream) -> TokenStream {
         panic!("supported only for async functions");
     }
 
+    let vis = &input.vis;
+    let args = &input.sig.inputs;
+    if args.len() != 2 {
+        panic!("handler must accept two arguments (D, HandlerInput)");
+    }
+
+    let data = if let Some(FnArg::Typed(ref ty)) = args.first() {
+        ty
+    } else {
+        panic!("app data type missing");
+    };
+
+    let p = &data.ty;
+
     let name = &input.sig.ident;
     let out = quote! {
+
         #[allow(non_camel_case_types)]
-        struct #name;
+        #vis struct #name;
 
         #[async_trait::async_trait]
-        impl Handler for #name {
-            async fn call(&self, input: HandlerInput) -> Result<HandlerOutput> {
+        impl Handler<#p> for #name
+        {
+            async fn call(&self, data: #p, input: HandlerInput) -> Result<HandlerOutput> {
                 #input
 
-                #name(input).await
+                #name(data, input).await
             }
         }
+
     };
 
     TokenStream::from(out)
